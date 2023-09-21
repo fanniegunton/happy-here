60b6e0e45f031374440acc0b4eebcb378d0ee123
Fannie Gunton <fanniegunton@gmail.com>
2023-09-21 16:29:50 -0500

    Update many site-wide styles

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -13,6 +13,7 @@ const colors = {
   hotPink: "#B13076",
   candyBlue: "#9FC9C2",
   sodaYellow: "#D8B85B",
+  fireRed: "#D14C5C",
 }
 
 const displayFontFamily = "Lato, Helvetica, sans-serif"
@@ -104,9 +105,14 @@ export default {
     fontFamily: displayFontFamily,
     fontSize: 20,
     fontWeight: 400,
-    lineHeight: 1.1,
+    lineHeight: 1.7,
     [breakpoints.tablet]: {
       fontSize: 18,
+      lineHeight: 1.5,
+    },
+    [breakpoints.smallMobile]: {
+      fontSize: 16,
+      lineHeight: 1.3,
     },
   },
 
@@ -139,6 +145,9 @@ export default {
   floatBox: {
     boxShadow: "var(--shadow-elevation-high)",
   },
+  extraFloatBox: {
+    boxShadow: "var(--shadow-elevation-extra)",
+  },
 }
 
 export const globalStyles = [
@@ -152,6 +161,8 @@ export const globalStyles = [
         "-0.2px 0.8px 0.9px hsl(var(--shadow-color) / 0.36), -0.8px 2.5px 3px -0.8px hsl(var(--shadow-color) / 0.36), -2px 6.3px 7.4px -1.7px hsl(var(--shadow-color) / 0.36), -4.8px 15.3px 18px -2.5px hsl(var(--shadow-color) / 0.36)",
       "--shadow-elevation-high":
         " -0.2px 0.8px 0.9px hsl(var(--shadow-color) / 0.34), -1.4px 4.5px 5.3px -0.4px hsl(var(--shadow-color) / 0.34), -2.6px 8.4px 9.9px -0.7px hsl(var(--shadow-color) / 0.34), -4.3px 13.7px 16.2px -1.1px hsl(var(--shadow-color) / 0.34), -6.9px 21.9px 25.8px -1.4px hsl(var(--shadow-color) / 0.34), -10.7px 34.3px 40.4px -1.8px hsl(var(--shadow-color) / 0.34), -16.3px 52.1px 61.4px -2.1px hsl(var(--shadow-color) / 0.34), -24px 76.7px 90.4px -2.5px hsl(var(--shadow-color) / 0.34)",
+      "--shadow-elevation-extra":
+        " -0.2px 0.8px 0.9px hsl(var(--shadow-color) / 0.34), -2.2px 6.5px 7px -0.2px hsl(var(--shadow-color) / 0.34), -3.4px 9.4px 9.9px -0.3px hsl(var(--shadow-color) / 0.34), -3.9px 11.2px 14.2px -1.1px hsl(var(--shadow-color) / 0.34), -6.9px 21.9px 25.8px -1.4px hsl(var(--shadow-color) / 0.34), -10.7px 34.3px 40.4px -1.8px hsl(var(--shadow-color) / 0.34), -16.3px 52.1px 61.4px -2.1px hsl(var(--shadow-color) / 0.34), -24px 76.7px 90.4px -2.5px hsl(var(--shadow-color) / 0.34)",
       "--headerBottomMargin": "0px",
       [breakpoints.tablet]: {
         "--headerBottomMargin": "0px",
@@ -161,13 +172,19 @@ export const globalStyles = [
       boxSizing: "inherit",
     },
     html: {
+      // Default font styles
       fontFamily: displayFontFamily,
       fontSize: 16,
       color: colors.black,
+      background: colors.white,
       lineHeight: 1.375,
       margin: 0,
       padding: 0,
+      border: 0,
       boxSizing: "border-box",
+      verticalAlign: "baseline",
+      fontStyle: "normal",
+      wordBreak: "break-word",
 
       // Improve browser font rendering
       textRendering: "optimizeLegibility",
@@ -177,15 +194,19 @@ export const globalStyles = [
       MozOsxFontSmoothing: "grayscale",
     },
 
+    img: {
+      maxWidth: "100%",
+    },
+
     "h1, h2, h3, h4, h5, h6": {
       margin: 0,
       fontSize: "inherit",
       lineHeight: "inherit",
     },
     body: {
-      // color: colors.loden,
-      // backgroundColor: colors.shell,
-      // background: "linear-gradient(90deg, #CDA564 0%, #E4AA8E 70%)",
+      color: colors.black,
+      backgroundColor: colors.white,
+      background: "linear-gradient(90deg, #CDA564 0%, #E4AA8E 70%)",
       background:
         "linear-gradient(90deg, #D28E79 0%, #CDA564 47.92%, #D28E79 98.44%)",
     },
@@ -194,8 +215,25 @@ export const globalStyles = [
       textDecoration: "none",
       boxSizing: "border-box",
     },
+    p: {
+      display: "inherit",
+      marginBlockStart: 0,
+      marginBlockEnd: 0,
+      marginInlineStart: 0,
+      marginInlineEnd: 0,
+    },
+    ul: {
+      listStyleType: "none",
+      marginBlockStart: 0,
+      marginBlockEnd: 0,
+      marginInlineStart: 0,
+      marginInlineEnd: 0,
+    },
     ".subtle-box": {
       boxShadow: "var(--shadow-elevation-low)",
+    },
+    ".notice-me-box": {
+      boxShadow: "var(--shadow-elevation-medium)",
     },
     ".in-your-face-box": {
       boxShadow: "var(--shadow-elevation-high)",
