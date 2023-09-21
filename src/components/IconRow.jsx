89f4d11158e7b85331989f6bc9ac08f3b542a290
Fannie Gunton <fanniegunton@gmail.com>
2023-09-21 16:28:55 -0500

    Add Icon Row functionality

---
+++ src/components/IconRow.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import PropTypes from "prop-types"
+import theme from "../styles/theme"
+
+const IconRow = ({
+  icon: Icon,
+  iconColor = theme.hotPink,
+  children,
+  className,
+}) => (
+  <div css={{ display: "flex", marginBottom: 8 }} className={className}>
+    <Icon
+      css={{
+        color: iconColor,
+        // marginRight: 8,
+        flex: "0 0 16px",
+      }}
+    />
+    <div>{children}</div>
+  </div>
+)
+
+export default IconRow
+
+IconRow.propTypes = {
+  icon: PropTypes.func.isRequired,
+  iconColor: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+}
