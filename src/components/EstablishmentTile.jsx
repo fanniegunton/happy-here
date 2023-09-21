84510049b3995d275660abadfc67f4048a28716a
Fannie Gunton <fanniegunton@gmail.com>
2023-09-21 16:37:19 -0500

    Change information layout, look, styles

--- src/components/EstablishmentTile.jsx
+++ src/components/EstablishmentTile.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react"
 import { graphql } from "gatsby"
 import theme from "../styles/theme"
-import HHStatusIndicator from "../components/HHStatusIndicator"
+import IconButton from "../components/IconButton"
+import IconRow from "../components/IconRow"
+import Icons from "../lib/icons"
+import { hoursCover } from "../lib/parseHours"
 
 const EstablishmentTile = ({
+  _id,
   name,
   address,
   neighborhood,
   website,
-  _id,
+  instagram,
   hours,
   happyHourTimes,
   happyHourDetails,
@@ -35,63 +39,138 @@ const EstablishmentTile = ({
     <div
       key={_id}
       css={{
-        padding: 30,
+        padding: "30px 0",
+        [theme.tablet]: {
+          padding: 20,
+        },
       }}
     >
       <div
         css={{
-          backgroundColor: theme.candyBlue,
+          padding: 40,
           backgroundColor: isOpen ? theme.sodaYellow : theme.candyBlue,
           borderRadius: 45,
           border: isOpen ? "16px double #B13076" : "2px solid #000",
+          maxWidth: 325,
+          ...theme.extraFloatBox,
+          [theme.smallMobile]: {
+            padding: 20,
+          },
         }}
       >
         <div
           css={{
             display: "flex",
-            justifyContent: "space-between",
             alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
           }}
         >
-          <h3 css={{ ...theme.h3 }}>{name}</h3>
-          <div
+          <h3
             css={{
-              display: "grid",
-              gridTemplateColumns: "1fr",
-              justifyContent: "center",
-              alignItems: "center",
+              ...theme.h3,
+              fontSize: 28,
+              textAlign: "center",
+              paddingBottom: "30px",
+              minWidth: 240,
+              [theme.tablet]: { marginBottom: 16 },
+              [theme.smallMobile]: {
+                fontSize: 24,
+                paddingBottom: 10,
+                minWidth: 200,
+              },
             }}
           >
-            <HHStatusIndicator
-              hours={happyHourTimes}
+            {name}
+          </h3>
+          {happyHourTimes && (
+            <div
               css={{
-                margin: 10,
-                width: 40,
-                height: 40,
-                borderRadius: 50,
+                marginBottom: 30,
+                fontSize: 18,
               }}
-            />
+            >
+              <IconRow
+                icon={Icons.Clock}
+                css={{ justifyContent: "center", fontSize: 14, marginRight: 0 }}
+              ></IconRow>
+              {happyHourTimes.map((line, index) => (
+                <div key={index}>{line}</div>
+              ))}
+            </div>
+          )}
+          {happyHourDetails?.includes("\n") ? (
+            <ul
+              css={{
+                listStyleType: "none",
+                paddingInlineStart: 0,
+                marginBottom: 30,
+              }}
+            >
+              {happyHourDetails
+                .split("\n")
+                .filter(Boolean)
+                .map((line, index) => (
+                  <li
+                    key={index}
+                    css={{ ...theme.bolded, fontSize: 14, textAlign: "left" }}
+                  >
+                    {line}
+                  </li>
+                ))}
+            </ul>
+          ) : (
+            <p css={{ ...theme.bolded, fontSize: 14, marginBottom: 24 }}>
+              {happyHourDetails}
+            </p>
+          )}
+          <div
+            css={{
+              textTransform: "capitalize",
+              fontSize: 12,
+              marginBottom: 24,
+            }}
+          >
+            <div css={{ marginBottom: 16 }}>
+              {address} <br />{" "}
+              <span css={{ fontStyle: "italic" }}>{neighborhood}</span>
+            </div>
+            <p css={{ fontSize: 12 }}>
+              <span css={{ ...theme.bolded }}>Hours of operation: </span>
+              {hours.map((line, index) => (
+                <div key={index}>{line}</div>
+              ))}
+            </p>
+          </div>
+          <div
+            css={{
+              [theme.tablet]: {
+                display: "flex",
+                justifyContent: "center",
+              },
+            }}
+          >
+            {website && (
+              <IconButton
+                icon={Icons.Website}
+                href={website}
+                target="_blank"
+                rel="noopener noreferrer"
+                css={{ width: "auto", marginRight: 40 }}
+              />
+            )}
+            {instagram && (
+              <IconButton
+                icon={Icons.Instagram}
+                href={instagram}
+                target="_blank"
+                rel="noopener noreferrer"
+                css={{ width: "auto" }}
+              />
+            )}
           </div>
         </div>
-        <p css={{ ...theme.bolded }}>{address}</p>
-        <p css={{ fontStyle: "italic" }}>{neighborhood} neighborhood</p>
-        <a href={website} target="_blank" rel="noopener noreferrer">
-          {website}
-        </a>
-        <p>
-          <span css={{ ...theme.bolded }}>Open Hours: </span>
-          {hours.map((line, index) => (
-            <div key={index}>{line}</div>
-          ))}
-        </p>
-        <p>
-          <span css={{ ...theme.bolded }}>Happy Hour Times: </span>{" "}
-          {happyHourTimes}
-        </p>
-        <p>
-          <span css={{ ...theme.bolded }}>Happy Hour Details: </span>
-          {happyHourDetails}
-        </p>
       </div>
     </div>
   )
@@ -101,11 +180,12 @@ export default EstablishmentTile
 
 export const query = graphql`
   fragment EstablishmentTile on SanityEstablishment {
+    _id
     name
     address
     neighborhood
     website
-    _id
+    instagram
     hours
     happyHourTimes
     happyHourDetails
