1754d18fc66d5e3ac9cdcc1ab05d98bcfa647c08
Fannie Gunton <fanniegunton@gmail.com>
2023-09-21 16:32:53 -0500

    Re-envision header display on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,94 +2,79 @@ import React from "react"
 import theme from "../styles/theme"
 
 const Header = () => (
-  <div>
-    <div
-      css={{
-        display: "flex",
-        justifyContent: "space-between",
-        paddingBottom: 60,
-        [theme.mobile]: {
-          justifyContent: "center",
-          flexDirection: "column",
-          paddingBottom: 20,
-        },
-      }}
-    >
-      <div>
-        <h1
-          css={{
-            ...theme.h1,
-
-            [theme.mobile]: {
-              ...theme.h1,
-              paddingBottom: 10,
-              textAlign: "center",
-            },
-          }}
-        >
-          Happy Here!
-        </h1>
-        <h4
-          css={{
-            ...theme.h4,
-
-            [theme.mobile]: {
-              ...theme.h4,
-              paddingBottom: 10,
-              textAlign: "center",
-            },
-          }}
-        >
-          Your friendly, neighborhood happy hour finder.
-        </h4>
-      </div>
-      <div
+  <div
+    css={{
+      display: "flex",
+      paddingBottom: 60,
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+    }}
+  >
+    <div>
+      <h1
         css={{
-          display: "flex",
-          justifyContent: "end",
-          alignItems: "center",
-          gridGap: 20,
+          ...theme.h1,
+          marginBottom: 20,
+          textAlign: "center",
           [theme.tablet]: {
-            flexDirection: "column",
-            justifyContent: "center",
-            padding: "0 20px",
-          },
-          [theme.mobile]: {
-            marginTop: 20,
-            flexDirection: "row",
-            justifyContent: "center",
+            ...theme.h1,
+            paddingBottom: 10,
           },
         }}
       >
-        <a
-          href="https://www.instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          css={{
-            ...theme.subtitle,
-            padding: "10px",
-            border: "2px solid black",
-            borderRadius: 50,
-            textAlign: "center",
-          }}
-        >
-          insta
-        </a>
-        <a
-          href="mailto:happyhappyhere@gmail.com"
-          css={{
-            ...theme.subtitle,
-            padding: "10px",
-            border: "2px solid black",
-            borderRadius: 50,
-            textAlign: "center",
-          }}
-        >
-          email
-        </a>
-      </div>
+        Happy Here!
+      </h1>
+      <h4
+        css={{
+          ...theme.subtitle,
+          paddingBottom: 40,
+          textAlign: "center",
+          maxWidth: 600,
+          [theme.tablet]: {
+            padding: "0 20px 40px",
+          },
+        }}
+      >
+        Your concierge finder of happy hours and other odd times of day when
+        drinks and food become less costly, and thus, more delicious. <br />{" "}
+      </h4>
     </div>
-    <div css={{ display: "flex", justifyContent: "space-between" }}></div>
+    <div
+      css={{
+        display: "flex",
+        gridGap: 20,
+      }}
+    >
+      <a
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        css={{
+          ...theme.subtitle,
+          fontFamily: theme.fancyFontFamily,
+          padding: "10px",
+          border: "2px solid black",
+          borderRadius: 50,
+          textAlign: "center",
+        }}
+      >
+        insta
+      </a>
+      <a
+        href="mailto:happyhappyhere@gmail.com"
+        css={{
+          ...theme.subtitle,
+          fontFamily: theme.fancyFontFamily,
+          padding: "10px",
+          border: "2px solid black",
+          borderRadius: 50,
+          textAlign: "center",
+        }}
+      >
+        email
+      </a>
+    </div>
   </div>
 )
 
