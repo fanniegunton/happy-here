c88d3a26ad5e1cf4c42b93dc5e1e5cee2d590450
Fannie Gunton <fanniegunton@gmail.com>
2025-02-22 18:59:56 -0600

    Initial Header w/ stubs for add'l nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,84 +1,98 @@
 import React from "react"
 import theme from "../styles/theme"
+import { Link } from "gatsby"
+import insta from "../images/Instagram.svg"
 
 const Header = () => (
   <div
     css={{
-      display: "flex",
-      paddingBottom: 60,
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
+      display: "block",
+      marginBottom: 60,
+      padding: "60px",
+      background:
+        "linear-gradient(135deg, #b5b5ff 0%, #e4e3e4 67.92%, #B13076 98.44%)",
+      borderRadius: "20px",
+      border: "16px double #4F67B1",
+      ...theme.extraFloatBox,
+      [theme.mobile]: {
+        padding: 10,
+      },
     }}
   >
-    <div>
+    <div
+      css={{
+        display: "grid",
+        gridTemplateColumns: "repeat(2, 1fr)",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 20,
+        [theme.tablet]: {
+          paddingBottom: 10,
+        },
+      }}
+    >
       <h1
         css={{
           ...theme.h1,
-          marginBottom: 20,
-          textAlign: "center",
-          [theme.tablet]: {
-            ...theme.h1,
-            paddingBottom: 10,
+        }}
+      >
+        Happy Here
+      </h1>
+      <div
+        css={{
+          ...theme.nav,
+          maxWidth: 600,
+          textWrap: "balance",
+          display: "flex",
+          gap: ".75em",
+          [theme.mobile]: {
+            display: "grid",
+            gridTemplateColumns: "repeat(2, 1fr)",
+            fontSize: 14,
+            textAlign: "right",
           },
         }}
       >
-        Happy Here!
-      </h1>
-      <h4
+        {/* <Link to="/lists">Lists</Link>
+        <Link to="/about">About</Link>
+        <Link to="/journal">Journal</Link> */}
+        <a href="mailto:happyhappyhere@gmail.com">Contact</a>
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={insta}
+            css={{
+              width: "auto",
+              color: theme.black,
+              [theme.mobile]: {
+                alignContent: "center",
+                margin: "0 auto",
+              },
+            }}
+          />
+        </a>
+      </div>
+    </div>
+    <div>
+      <div
         css={{
           ...theme.subtitle,
-          paddingBottom: 40,
-          textAlign: "center",
           maxWidth: 600,
+          textWrap: "balance",
           [theme.tablet]: {
-            padding: "0 20px 40px",
+            padding: "0 20px 20px 0",
+            fontWeight: 400,
           },
         }}
       >
-        Your concierge finder of happy hours and other odd times of day when
-        drinks and food become less costly, and thus, more delicious. <br />{" "}
-      </h4>
+        Your roadmap to delicious discounts, from early happy hours to late
+        night deals.
+        <br />{" "}
+      </div>
     </div>
-    {/* <div
-      css={{
-        display: "flex",
-        gridGap: 20,
-      }}
-    > */}
-    <a
-      href="https://www.instagram.com"
-      target="_blank"
-      rel="noopener noreferrer"
-      css={{
-        ...theme.subtitle,
-        fontFamily: theme.fancyFontFamily,
-        padding: "10px",
-        border: "2px solid black",
-        borderRadius: 50,
-        textAlign: "center",
-      }}
-    >
-      insta
-    </a>
-    <p css={{ margin: "20px 0" }}>
-      We are always seeking and reviewing new HH spots! If you have a
-      suggestion, drop us a line:
-    </p>
-    <a
-      href="mailto:happyhappyhere@gmail.com"
-      css={{
-        ...theme.subtitle,
-        fontFamily: theme.fancyFontFamily,
-        padding: "10px",
-        border: "2px solid black",
-        borderRadius: 50,
-        textAlign: "center",
-      }}
-    >
-      email
-    </a>
-    {/* </div> */}
   </div>
 )
 
