cc640278db463a9d921005e04eca428533e6e237
Fannie Gunton <fanniegunton@gmail.com>
2025-04-25 17:29:21 -0500

    Updates to home page with filter bar including entirety of logic

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,13 +16,140 @@ const Home = ({ data }) => {
     )
   }
 
+  const [hasWine, setHasWine] = React.useState(false)
+  const [hasBeer, setHasBeer] = React.useState(false)
+  const [hasCocktails, setHasCocktails] = React.useState(false)
+  const [hasFood, setHasFood] = React.useState(false)
+  const [hasCoffee, setHasCoffee] = React.useState(false)
+  const [hasPatio, setHasPatio] = React.useState(false)
+  const [hasBarSeating, setHasBarSeating] = React.useState(false)
+  const [hasDogFriendly, setHasDogFriendly] = React.useState(false)
+  const [hasBigGroups, setHasBigGroups] = React.useState(false)
+
   const sortedEstablishments = sortEstablishments(establishments)
+
+  const filteredEstablishments = sortedEstablishments.filter((est) => {
+    if (hasWine && !est.whatWeHaveHere.includes("wine")) return false
+    if (hasBeer && !est.whatWeHaveHere.includes("beer")) return false
+    if (hasCocktails && !est.whatWeHaveHere.includes("cocktails")) return false
+    if (hasFood && !est.whatWeHaveHere.includes("food")) return false
+    if (hasCoffee && !est.whatWeHaveHere.includes("coffee")) return false
+    if (hasPatio && !est.theSpaceIsLike.includes("patio")) return false
+    if (hasBarSeating && !est.theSpaceIsLike.includes("barSeating"))
+      return false
+    if (hasDogFriendly && !est.theSpaceIsLike.includes("dogFriendly"))
+      return false
+    if (hasBigGroups && !est.theSpaceIsLike.includes("bigGroups")) return false
+
+    return true // only return true if it passed all active filters
+  })
 
   return (
     <div>
       <Layout>
         <Global styles={globalStyles} />
         <Header css={{ margin: "0 auto" }} />
+        <div
+          css={{
+            margin: "0 auto 10px",
+            padding: "0 20px",
+            ...theme.postTitle,
+            fontWeight: 700,
+            [theme.mobile]: {
+              margin: "0 30px 10px",
+              padding: 0,
+            },
+          }}
+        >
+          Only show me places with:
+        </div>
+        <div
+          css={{
+            margin: "0 auto",
+            padding: "0 20px",
+            ...theme.subtitle,
+            [theme.mobile]: {
+              margin: "0 30px",
+              padding: 0,
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr",
+            },
+          }}
+        >
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasWine(!hasWine)}
+            />
+            Wine
+          </label>
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasBeer(!hasBeer)}
+            />
+            Beer
+          </label>
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasCocktails(!hasCocktails)}
+            />
+            Cocktails
+          </label>
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasFood(!hasFood)}
+            />
+            Food
+          </label>
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasCoffee(!hasCoffee)}
+            />
+            Coffee
+          </label>
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasPatio(!hasPatio)}
+            />
+            Patio
+          </label>
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasBarSeating(!hasBarSeating)}
+            />
+            Bar Seating
+          </label>
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasBigGroups(!hasBigGroups)}
+            />
+            Room for 4 or more
+          </label>
+          <label css={{ marginRight: 20 }}>
+            <input
+              css={{ marginRight: 10 }}
+              type="checkbox"
+              onChange={() => setHasDogFriendly(!hasDogFriendly)}
+            />
+            🐶🐾 Friendly //{" "}
+          </label>{" "}
+          */}
+        </div>
         <div
           css={{
             display: "grid",
@@ -41,7 +168,7 @@ const Home = ({ data }) => {
             [theme.mobile]: { margin: 0 },
           }}
         >
-          {sortedEstablishments.map((tile) => (
+          {filteredEstablishments.map((tile) => (
             <EstablishmentTile key={tile._id} {...tile} />
           ))}
         </div>
