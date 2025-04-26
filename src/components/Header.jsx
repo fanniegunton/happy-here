a068d01f3a409bd6b4269fd9dd8714684728dd91
Fannie Gunton <fanniegunton@gmail.com>
2025-04-25 20:16:10 -0500

    Split out separate Nav component from Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
 import theme from "../styles/theme"
-import insta from "../images/Instagram.svg"
 
 const Header = () => (
   <div
@@ -17,9 +15,6 @@ const Header = () => (
   >
     <div
       css={{
-        display: "flex",
-        alignItems: "start",
-        justifyContent: "space-between",
         marginBottom: 20,
         [theme.tablet]: {
           paddingBottom: 10,
@@ -43,70 +38,26 @@ const Header = () => (
       >
         Happy Here: Happy Hours in Austin
       </h1>
-      <div
-        css={{
-          ...theme.nav,
-          paddingTop: 40,
-          flex: "1 0 auto",
-          maxWidth: 600,
-          textWrap: "balance",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "end",
-          transform: "0 16px",
-          gap: ".75em",
-          [theme.mobile]: {
-            marginTop: 20,
-            justifyContent: "start",
-            background: theme.lilac,
-            marginRight: "-30px",
-            marginLeft: "-30px",
-            padding: "10px 0 10px 30px",
-          },
-        }}
-      >
-        {/* <Link to="/lists">Lists</Link> */}
-        {/* <Link to="/about">About</Link> */}
-        {/* <Link to="/journal">Journal</Link> */}
-        <a href="mailto:happyhappyhere@gmail.com">Contact</a>
-        <a
-          href="https://www.instagram.com/takeouttracker/"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <div
+          css={{
+            ...theme.subtitle,
+            fontWeight: 400,
+            maxWidth: 720,
+            textWrap: "balance",
+            [theme.tablet]: {
+              padding: "0 20px 30px 0",
+            },
+            [theme.mobile]: {
+              padding: "0 0 30px 0",
+              fontSize: 16,
+              lineHeight: 1.5,
+            },
+          }}
         >
-          <img
-            src={insta}
-            css={{
-              width: "auto",
-              color: theme.black,
-              [theme.mobile]: {
-                alignContent: "end",
-                marginLeft: 8,
-              },
-            }}
-          />
-        </a>
-      </div>
-    </div>
-    <div>
-      <div
-        css={{
-          ...theme.subtitle,
-          fontWeight: 400,
-          maxWidth: 720,
-          textWrap: "balance",
-          [theme.tablet]: {
-            padding: "0 20px 30px 0",
-          },
-          [theme.mobile]: {
-            padding: "0 0 30px 0",
-            fontSize: 16,
-            lineHeight: 1.5,
-          },
-        }}
-      >
-        Delicious discounts in ATX! <br /> Happy hours, late night deals,
-        specialty days, plus all the deets 🤌🏼
+          Delicious discounts in ATX! <br /> Happy hours, late night deals,
+          specialty days, plus all the deets 🤌🏼
+        </div>
       </div>
     </div>
   </div>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,82 @@
+import React from "react"
+import { Link } from "gatsby"
+import theme from "../styles/theme"
+import insta from "../images/Instagram.svg"
+import photo from "../assets/HH-logo.jpg"
+
+const Nav = () => {
+  return (
+    <div
+      css={{
+        margin: "0 auto 60px",
+        padding: "0 20px",
+        display: "flex",
+        alignItems: "start",
+        justifyContent: "space-between",
+        [theme.mobile]: {
+          padding: 0,
+        },
+      }}
+    >
+      <Link to="/" css={{ maxWidth: "65%" }}>
+        {photo && (
+          <img
+            src={photo}
+            alt="Happy Here logo with a happy hour drink"
+            css={{
+              display: "block",
+              width: 150,
+              objectFit: "cover",
+              borderRadius: 90,
+            }}
+          />
+        )}
+      </Link>
+      <div
+        css={{
+          ...theme.nav,
+          paddingTop: 40,
+          flex: "1 0 auto",
+          maxWidth: 600,
+          textWrap: "balance",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "end",
+          transform: "0 16px",
+          gap: 40,
+          [theme.mobile]: {
+            marginTop: 20,
+            justifyContent: "start",
+            background: theme.lilac,
+            marginRight: "-30px",
+            marginLeft: "-30px",
+            padding: "10px 0 10px 30px",
+          },
+        }}
+      >
+        {/* <Link to="/lists">Lists</Link> */}
+        <Link to="/about">About</Link>
+        {/* <Link to="/journal">Journal</Link> */}
+        <a href="mailto:happyhappyhere@gmail.com">Contact</a>
+        <a
+          href="https://www.instagram.com/takeouttracker/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={insta}
+            css={{
+              width: "auto",
+              color: theme.black,
+              [theme.mobile]: {
+                alignContent: "end",
+                marginLeft: 8,
+              },
+            }}
+          />
+        </a>
+      </div>
+    </div>
+  )
+}
+export default Nav
